010a89bcd212c6ebaf50271e1fc1237f199d9ecf
Meinte Boersma <meinte.boersma@gmail.com>
2011-11-01 15:12:39 +0100

    improved grammars by using terminal fragments + added validation to RGB hex colors

--- nl.dslmeinte.xtext.css/src/nl/dslmeinte/xtext/css/services/CSSValueConverterService.java
+++ nl.dslmeinte.xtext.css/src/nl/dslmeinte/xtext/css/services/CSSValueConverterService.java
@@ -3,6 +3,7 @@ package nl.dslmeinte.xtext.css.services;
 import org.eclipse.xtext.common.services.DefaultTerminalConverters;
 import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
+import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.nodemodel.INode;
 
 public class CSSValueConverterService extends DefaultTerminalConverters {
@@ -31,6 +32,24 @@ public class CSSValueConverterService extends DefaultTerminalConverters {
 			@Override
 			public String toValue(String string, INode node) {
 				return string.substring(1);
+			}
+		};
+	}
+
+	@ValueConverter(rule="RGB_COLOR")
+	public IValueConverter<String> RGB_COLOR() {
+		return new IValueConverter<String>() {
+			@Override
+			public String toString(String value) {
+				return "#" + value;
+			}
+			@Override
+			public String toValue(String string, INode node)
+					throws ValueConverterException {
+				if( string.length() == 4 || string.length() == 7 ) {
+					return string.substring(1);
+				}
+				throw new ValueConverterException("'" + string + "' is not a valid RGB color code", node, null);
 			}
 		};
 	}
